16b47847d8eeb0e53645c6839e3a502a4d44d998
Jose Macedo <josemacedowork@gmail.com>
2026-03-18 21:19:47 -0300

    feat: aula 2 js

---
+++ DEV-QUEST-2X0/js/aula_02-tipos-primitivos/index.js
@@ -0,0 +1,14 @@
+// são todos os tipos mais comuns de dados que exitem como
+
+let string = "opa" //qualquer tipo de carcter que for colocado entre aspas ""
+let bool = true //um tipo unico de dado que tem como carcteristica apenas dizer se é verdadeiro ou falso no caso true ou false
+let num = 22 // no caso do js estão inclusos tanto o float como os int em uma classe só a Number, sempre escrita com "N" maiusculo
+let nulo = null // significa que o valor da variavel é nulo mas ainda pode vir a existir
+let indef = undefined // significa que o valor não foi definido
+
+
+console.log(string)
+console.log(bool)
+console.log(num)
+console.log(nulo)
+console.log(indef)
